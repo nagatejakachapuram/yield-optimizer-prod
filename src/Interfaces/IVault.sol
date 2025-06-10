c5f1703e158c4167a856a49e156e12a7a7b65eb6
Nagateja kachapuram <104291208+nagatejakachapuram@users.noreply.github.com>
2025-06-10 13:51:56 +0530

    Update IVault.sol

--- src/Interfaces/IVault.sol
+++ src/Interfaces/IVault.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.28;
 
-// Suggested interface if Chainlink calls Vault
+
 interface IVault {
     function allocateFunds(address user, uint256 amount, address strategy) external;
 }
